1433f791e7169e0c5c6912f3cb8033187522bba6
prayansh <parmarprayansh2@gmail.com>
2024-01-16 17:40:00 +0530

    Prisma Setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import express, {Express, Request, Response} from 'express';
-import { ApolloServer} from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import axios from 'axios';
-
+import express, { Express, Request, Response } from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import cors from "cors";
+import axios from "axios";
+import { prismaClient } from "./lib/db";
 const PORT = Number(process.env.PORT) || 8000;
-const app:Express = express();
+const app: Express = express();
 
 async function startServer() {
-    const  server = new ApolloServer({
-        typeDefs: `
+  const server = new ApolloServer({
+    typeDefs: `
 
         type User {
             id: ID!
@@ -32,34 +32,69 @@ async function startServer() {
             getAllUsers: [User]
             getUser(id: ID!): User
         }
-        `,
-        resolvers: {
-            Todo:{
-                user: async (todo) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.id}`)).data,
-            },
-            Query: {
-                getTodos: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
-                getAllUsers: async () => (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
-                getUser: async (parent,{id}) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data,
-                
-            }
+
+        type Mutation {
+            createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
         }
-    });
-    
-    app.use(bodyParser.json());
-    app.use(cors());
-    app.use(express.json());
+        `,
+    resolvers: {
+      Todo: {
+        user: async (todo) =>
+          (
+            await axios.get(
+              `https://jsonplaceholder.typicode.com/users/${todo.id}`
+            )
+          ).data,
+      },
+      Query: {
+        getTodos: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+        getAllUsers: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+        getUser: async (parent, { id }) =>
+          (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`))
+            .data,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "salt",
+            },
+          });
+          return true;
+        },
+      },
+    },
+  });
 
-    await server.start();
-    app.use('/graphql',expressMiddleware(server));
-    app.listen(PORT,()=> console.log(`Server running on port: ${PORT}`)
-    )
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(express.json());
 
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 }
 
 startServer();
-
-
 
 // app.get('/',(req: Request, res: Response) => {
 //     res.json("Hy");
@@ -72,5 +107,3 @@ startServer();
 // app.listen(PORT, () => {
 //     console.log(`Server running on Port: ${PORT}`);
 // })
-
-
